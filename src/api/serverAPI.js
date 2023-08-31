67e555c7a3cacfe08d38ebd0d5eb4620ee877d80
LeoDm123 <leo.meiners1@gmail.com>
2023-08-31 09:17:34 -0300

    URL

--- src/api/serverAPI.js
+++ src/api/serverAPI.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const serverAPI = axios.create({
-  baseURL: "http://localhost:10000",
+  baseURL:
+    "mongodb+srv://leomeiners1:6SWQ3vV8PJudTm82@portaldm.a5dtqas.mongodb.net/Exchange",
 });
 
 export default serverAPI;
