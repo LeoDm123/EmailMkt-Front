2526343560f715c27abcb5218a0bc8d384722488
LeoDm123 <leo.meiners1@gmail.com>
2023-08-31 08:41:45 -0300

    URL

--- src/api/serverAPI.js
+++ src/api/serverAPI.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const serverAPI = axios.create({
-  baseURL: "http://localhost:4040",
+  baseURL:
+    "https://sa-east-1.aws.data.mongodb-api.com/app/data-vigln/endpoint/data/v1",
 });
 
 export default serverAPI;
