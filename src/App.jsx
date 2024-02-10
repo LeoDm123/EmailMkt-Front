1305af18af6ec413bed74b13c74f9903f8b4cd00
LeoDm123 <leo.meiners1@gmail.com>
2024-02-09 21:07:07 -0300

    cambio de browser to hash

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Landing from "./pages/landing";
 import AdminDashboard from "./pages/admin/adminDashboard";
 import AdminPanel from "./pages/admin/adminPanel";
@@ -6,14 +6,14 @@ import ClientDashboard from "./pages/client/clientDashboard";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/a/dashboard" element={<AdminDashboard />} />
         <Route path="/a/panel" element={<AdminPanel />} />
         <Route path="/c/dashboard" element={<ClientDashboard />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
